9cc3f5113c9f00dcc1318b0c3643d5a93284cedb
Sanghyeon Lee <isseebx38@gmail.com>
2018-04-07 16:34:31 +0900

    fin

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import './App.css';
 import SideBar from './SideBar'
 import Content from './Content'
+import Clients from './Clients'
+import Services from './Services'
+import Contact from './Contact'
+import Home from './Home'
+import About from './About'
 
 class App extends Component {
   constructor(props){
@@ -17,8 +22,26 @@ class App extends Component {
     });
   }
 
-  render() {
+  _getPage(){
     const {page} = this.state;
+    if(page === 'Home') {
+      return <Home />;
+    }
+    else if(page === 'About') {
+      return <About />;
+    }
+    else if(page === 'Clients') {
+      return <Clients />;
+    }
+    else if(page === 'Services') {
+      return <Services />;
+    }
+    else if(page === 'Contact') {
+      return <Contact />;
+    }
+  }
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -27,7 +50,7 @@ class App extends Component {
           </h1>
         </header>
         <SideBar onPageSelect={(page)=>{this._setPage(page)}}/>
-        <Content page={page} />
+        <Content page={this._getPage()}/>
         <footer className="App-footer">
           Chungnam National Univ <code>src/App.js</code>
         </footer>
--- src/Content.js
+++ src/Content.js
@@ -4,7 +4,7 @@ import './Content.css'
 class Content extends Component {
     render() {
         return (
-            <div clasName="App-content">
+            <div className="App-content">
                 {this.props.page}
             </div>
         );
